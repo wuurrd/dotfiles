3e3f98b864bc85621d9dcd033c6375503b6b07e3
David Buchmann <davbuchm@cisco.com>
2012-10-23 11:20:17 +0200

    Add urxvt clipboard support.

--- install.sh
+++ install.sh
@@ -13,3 +13,4 @@ sudo ln -s `pwd`/xmodmap ~/.xmodmap
 sudo ln -s `pwd`/profile ~/.profile
 sudo ln -s `pwd`/Xdefaults ~/.Xdefaults
 sudo ln -s `pwd`/weechat ~/.weechat
+sudo ln -s `pwd`/clipboard.urxvt /usr/lib/urxvt/perl/clipboard
