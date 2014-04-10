4493c532c5e64f1aab6a2759e89c4517aa5d68ae
David Buchmann <davbuchm@cisco.com>
2014-04-10 22:40:14 +0200

    Cleanup install script

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
+pushd .
 mkdir -p ~/src
 cd ~/src
 git clone git@github.com:zsh-users/zaw.git
@@ -7,24 +8,34 @@ git clone git@github.com:zsh-users/zsh-history-substring-search.git
 cd ~
 git clone git://github.com/robbyrussell/oh-my-zsh.git
 
+popd # back to ~ (in zsh cd'ing popd / pushd's)
+popd # back to dotfiles
 #git clone https://github.com/pdf/ubuntu-mono-powerline-ttf.git ~/.fonts/ubuntu-mono-powerline-ttf
 #fc-cache -vf
 
-#sudo ln -s `pwd`/irssi ~/.irssi
-#sudo ln -s `pwd`/fluxbox ~/.fluxbox
-#sudo ln -s `pwd`/awesome ~/.config/awesome
-#sudo ln -s `pwd`/nitrogen ~/.config/nitrogen
-#sudo ln -s `pwd`/vimrc ~/.vimrc
-#sudo ln -s `pwd`/init.el ~/.emacs
-#sudo ln -s `pwd`/inputrc ~/.inputrc
+#ln -s `pwd`/irssi ~/.irssi
+
+###### LINUX ######
+#ln -s `pwd`/fluxbox ~/.fluxbox
+#ln -s `pwd`/awesome ~/.config/awesome
+#ln -s `pwd`/nitrogen ~/.config/nitrogen
+#ln -s `pwd`/xmodmap ~/.xmodmap
+#ln -s `pwd`/profile ~/.profile
+#ln -s `pwd`/Xdefaults ~/.Xdefaults
+#ln -s `pwd`/clipboard.urxvt /usr/lib/urxvt/perl/clipboard
+####################
+
+ln -s `pwd`/inputrc ~/.inputrc
+ln -s `pwd`/init.el ~/.emacs
 ln -s `pwd`/tmux.conf ~/.tmux.conf
-#sudo ln -s `pwd`/xmodmap ~/.xmodmap
-#sudo ln -s `pwd`/profile ~/.profile
-#sudo ln -s `pwd`/Xdefaults ~/.Xdefaults
 ln -s `pwd`/weechat ~/.weechat
 ln -s `pwd`/.zshrc ~/.zshrc
-ln -s `pwd`/.vimrc ~/.vimrc
-#sudo ln -s `pwd`/clipboard.urxvt /usr/lib/urxvt/perl/clipboard
 
 
+### VIM CONFIG ###
 curl -Lo- https://bit.ly/janus-bootstrap | bash
+cd ~/.vim/janus/vim/tools
+git clone https://github.com/bling/vim-airline.git
+cd -
+ln -s `pwd`/.vimrc ~/.vimrc
+##################
