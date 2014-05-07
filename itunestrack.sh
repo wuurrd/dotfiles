ec202f45f56ee420e59a61b0babb8dacdd2af788
David Buchmann <davbuchm@cisco.com>
2014-05-07 14:42:39 +0200

    Fixing itunestrack

--- itunestrack.sh
+++ itunestrack.sh
@@ -1,29 +1,37 @@
 #!/usr/bin/env bash
 ITUNES_TRACK=$(osascript <<EOF
+set trackname to missing value
 tell application "iTunes"
   if player state is playing then
-    get the name of the current track
+    set trackname to name of the current track
   end if
 end tell
+
 tell application "Spotify"
   if player state is playing then
-    get the name of the current track
+    set trackname to name of the current track
   end if
 end tell
+
+trackname
 EOF)
  
-if test "x$ITUNES_TRACK" != "x"; then
+if test "x$ITUNES_TRACK" != "xmissing value"; then
 ITUNES_ARTIST=$(osascript <<EOF
+set trackname to missing value
 tell application "iTunes"
   if player state is playing then
-    get the artist of the current track 
+    set trackname to artist of the current track
   end if
 end tell
+
 tell application "Spotify"
   if player state is playing then
-    get the artist of the current track 
+    set trackname to artist of the current track
   end if
 end tell
+
+trackname
 EOF)
  
     echo '♫' $ITUNES_TRACK '-' $ITUNES_ARTIST
