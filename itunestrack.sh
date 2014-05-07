8d655fe7dbed227a14ae826e21605d8fbf6990b7
David Buchmann <davbuchm@cisco.com>
2014-05-07 14:28:23 +0200

    Fixing itunestrack to also work with spotify

--- itunestrack.sh
+++ itunestrack.sh
@@ -2,6 +2,8 @@
 ITUNES_TRACK=$(osascript <<EOF
 if appIsRunning("iTunes") then
     tell app "iTunes" to get the name of the current track
+else if appIsRunning("Spotify") then
+    tell app "Spotify" to get the name of the current track
 end if
  
 on appIsRunning(appName)
@@ -13,7 +15,10 @@ if test "x$ITUNES_TRACK" != "x"; then
 ITUNES_ARTIST=$(osascript <<EOF
 if appIsRunning("iTunes") then
     tell app "iTunes" to get the artist of the current track
+else if appIsRunning("Spotify") then
+    tell app "Spotify" to get the artist of the current track
 end if
+
  
 on appIsRunning(appName)
     tell app "System Events" to (name of processes) contains appName
