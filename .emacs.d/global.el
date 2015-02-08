baa8d222975b960b9a95835ac0cb70f015b05613
David Buchmann <david@pexip.com>
2015-02-08 12:17:14 +0100

    Make magit fullscreen

--- .emacs.d/global.el
+++ .emacs.d/global.el
@@ -139,3 +139,18 @@
 (require 'javascript)
 (global-set-key (kbd "M-i") 'change-inner)
 (global-set-key (kbd "M-o") 'change-outer)
+
+;; full screen magit-status
+(require 'magit)
+(defadvice magit-status (around magit-fullscreen activate)
+  (window-configuration-to-register :magit-fullscreen)
+  ad-do-it
+  (delete-other-windows))
+
+(defun magit-quit-session ()
+  "Restores the previous window configuration and kills the magit buffer"
+  (interactive)
+  (kill-buffer)
+  (jump-to-register :magit-fullscreen))
+
+(define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
