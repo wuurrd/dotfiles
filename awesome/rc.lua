340257609d339db3c6620c2198d3a5fa3751070b
David Buchmann <david.buchmann@gmail.com>
2015-07-18 13:02:03 +0200

    add menu and mail fixes

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -377,6 +377,7 @@ end
 
 -- {{{ Mouse bindings
 root.buttons(awful.util.table.join(
+    awful.button({ }, 1, function () mymainmenu:hide() end),
     awful.button({ }, 3, function () mymainmenu:toggle() end),
     awful.button({ }, 4, awful.tag.viewnext),
     awful.button({ }, 5, awful.tag.viewprev)
--- conky/unread-gmail.py
+++ conky/unread-gmail.py
@@ -1,8 +1,8 @@
 import imaplib
 import keyring
-obj = imaplib.IMAP4_SSL('imap.gmail.com','993')
-obj.login('david.buchmann@gmail.com',
-          keyring.get_password('gmail', 'david.buchmann@gmail.com'))
-obj.select()
-obj.search(None,'UnSeen')
-print len(obj.search(None, 'UnSeen')[1][0].split())
+work = imaplib.IMAP4_SSL('imap.gmail.com','993')
+work.login('david@pexip.com',
+          keyring.get_password('gmail', 'david@pexip.com'))
+work.select()
+work.search(None,'UnSeen')
+print len(work.search(None, 'UnSeen')[1][0].split())
