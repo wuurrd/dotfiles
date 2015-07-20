3214186fddabde9c4df899df913d36c35f0b818b
David Buchmann <david.buchmann@gmail.com>
2015-07-20 10:37:19 +0200

    Package initialize

--- init.el
+++ init.el
@@ -12,6 +12,13 @@
 ;;      soul."
 ;;                                                          [Per Abrahamsen]
 ;;
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (add-to-list 'load-path (expand-file-name "/usr/share/emacs/site-lisp/emacs-goodies-el/"))
 (add-to-list 'load-path (expand-file-name "~/dotfiles/.emacs.d/yasnippet"))
 (add-to-list 'load-path (expand-file-name "~/dotfiles/.emacs.d"))
