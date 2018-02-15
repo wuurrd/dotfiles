2b89c3f40f22ec8d78eee141f5214caaf37893d3
David Buchmann <david@jazznetworks.com>
2018-02-15 17:16:19 +0100

    global: Add toolbar (not really - tricked you emacs rendering bug)

--- .emacs.d/global.el
+++ .emacs.d/global.el
@@ -6,7 +6,6 @@
 (fset 'yes-or-no-p 'y-or-n-p)
 
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
-(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
 ;Buffer settings
@@ -21,27 +20,6 @@
   "Select the previous window."
   (interactive)
   (other-window -1))
-
-(require 'auto-install)
-
-
-(custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(ecb-options-version "2.40")
- '(ecb-source-path (quote ("~/src")))
- '(ecb-tip-of-the-day nil)
- '(ecb-windows-width 0.15)
- '(enable-local-variables :all)
- '(revert-without-query (quote (".*\\.pdf"))))
-
-(setq org-todo-keywords (quote ((sequence "TODO(t!)" "NEXT(n!)" "|" "DONE(d!/!)")
-                                (sequence "WAITING(w@/!)" "SOMEDAY(s!)" "|" "CANCELLED(c@/!)")
-                                (sequence "QUOTE(q!)" "QUOTED(Q!)" "|" "APPROVED(A@)" "EXPIRED(E@)" "REJECTED(R@)")
-                                (sequence "OPEN(O!)" "|" "CLOSED(C!)"))))
-
 
 (blink-cursor-mode (- (*) (*) (*)))
 ;(toggle-show-tabs-show-ws)
