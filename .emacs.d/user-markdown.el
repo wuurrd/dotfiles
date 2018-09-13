8b50da6ee8df75a9912d116ee1f1ea5ec81f326b
David Buchmann <david@jazznetworks.com>
2018-09-13 18:19:40 +0300

    emacs: org-mode notes

--- .emacs.d/user-markdown.el
+++ .emacs.d/user-markdown.el
@@ -7,10 +7,15 @@
   :init (setq markdown-command "multimarkdown")
 )
 
+(defun open-org()
+  (interactive)
+  (find-file "~/organizer.org")
+)
+
 (use-package org :ensure t
   :commands org-mode
   :init
-  (setq org-startup-indented t)
+  (setq org-startup-indented nil)
   (setq org-M-RET-may-split-line nil)
   (setq org-src-fontify-natively t)
   (setq org-latex-listings 'minted)
@@ -30,9 +35,15 @@
         '("pdflatex -interaction nonstopmode -shell-escape -output-directory %o %f"
           "pdflatex -interaction nonstopmode -shell-escape -output-directory %o %f"
           "pdflatex -interaction nonstopmode -shell-escape -output-directory %o %f"))
+  (setq org-default-notes-file "~/organizer.org")
   :config
   (add-hook 'org-mode-hook (lambda () (auto-revert-mode 1)))
   (add-to-list 'org-latex-packages-alist '("" "minted"))
   (add-to-list 'org-file-apps '(system . "xdg-open \"%s\""))
   (add-to-list 'org-file-apps '(t . "xdg-open \"%s\""))
+  :bind (
+    :map global-map
+    ("C-c o" . 'open-org)
+    ("C-c c" . 'org-capture)
+  )
 )
