1e6661a570f45cff3bc988e34ee87d7c6765e58d
David Buchmann <david@jazznetworks.com>
2018-09-13 17:03:34 +0200

    org-mode

--- .emacs.d/user-markdown.el
+++ .emacs.d/user-markdown.el
@@ -6,3 +6,33 @@
          ("\\.markdown\\'" . markdown-mode))
   :init (setq markdown-command "multimarkdown")
 )
+
+(use-package org :ensure t
+  :commands org-mode
+  :init
+  (setq org-startup-indented t)
+  (setq org-M-RET-may-split-line nil)
+  (setq org-src-fontify-natively t)
+  (setq org-latex-listings 'minted)
+  (setq org-log-done 'time)
+  (setq org-todo-keywords
+        '((sequence
+           "TODO(t)"
+           "NEXT(n)"
+           "WAITING(w@/!)"
+           "ONGOING(o)"
+           "|"
+           "DONE(d)"
+           "CANCELLED(c@)"
+           ))
+        )
+  (setq org-latex-pdf-process
+        '("pdflatex -interaction nonstopmode -shell-escape -output-directory %o %f"
+          "pdflatex -interaction nonstopmode -shell-escape -output-directory %o %f"
+          "pdflatex -interaction nonstopmode -shell-escape -output-directory %o %f"))
+  :config
+  (add-hook 'org-mode-hook (lambda () (auto-revert-mode 1)))
+  (add-to-list 'org-latex-packages-alist '("" "minted"))
+  (add-to-list 'org-file-apps '(system . "xdg-open \"%s\""))
+  (add-to-list 'org-file-apps '(t . "xdg-open \"%s\""))
+)
